da969942b03aded70eec3ed8f00235f258097d85
SeoPang <croderknight@gmail.com>
2022-11-22 01:15:58 +0900

    detail modification

--- LectureSchduler/LectureSchduler.c
+++ LectureSchduler/LectureSchduler.c
@@ -8,7 +8,7 @@ int main()
 {
 	Schedule sche = { {0,}, {0,}, {0,}, {0,}, {0,} };
 	Lecture lec;
-	int lec_index[MAX_LECTURE] = { 0, };
+	int lec_index_array[MAX_LECTURE] = { 0, }; //구조체의 인덱스 번호만 있는 array,
 	int idx = 0;
 
 	
@@ -20,7 +20,13 @@ int main()
 
 		if (!strcmp(lec.name, "교육통계"))
 		{
-			lec_index[idx] = i;
+			lec_index_array[idx] = i;
+			idx++;
+		}
+
+		if (!strcmp(lec.name, "공간에대한인문학적이해"))
+		{
+			lec_index_array[idx] = i;
 			idx++;
 		}
 	}
@@ -29,14 +35,15 @@ int main()
 
 	for (int i = 0; i < idx; i++)
 	{
-		lec = lec_search(lec_index[i]); //원하는 인덱스를 삽입
+		lec = lec_search(lec_index_array[i]); //원하는 인덱스를 삽입
 		
 		lec_print(lec);
 	}
 
-	lec = lec_search(lec_index[0]);
+	lec = lec_search(lec_index_array[0]);
 	sche = push_lec(lec, sche);
-	lec_print(lec);
+	lec = lec_search(lec_index_array[1]);
+	sche = push_lec(lec, sche);
 
 	sche_print(sche);
 
@@ -48,13 +55,5 @@ int main()
 	return 0;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+//나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
